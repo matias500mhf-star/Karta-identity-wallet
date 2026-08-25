d03a376b3d508e12900622e037677296a30f3ea1
matias500mhf-star <matias500.mhf@gmail.com>
2026-08-25 11:50:11 +0200

    feat(api): add documents module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { DocumentsModule } from './documents/documents.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, HealthModule],
+  imports: [DatabaseModule, AuthModule, DocumentsModule, HealthModule],
 })
 export class AppModule {}
