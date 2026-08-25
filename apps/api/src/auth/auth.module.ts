c88a4744d9e70bac20b855db0d1c2e8cd3cc2e76
matias500mhf-star <matias500.mhf@gmail.com>
2026-08-25 11:48:28 +0200

    feat(auth): register token service

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
+import { TokenService } from './token.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PasswordService],
-  exports: [AuthService, PasswordService],
+  providers: [AuthService, PasswordService, TokenService],
+  exports: [AuthService, PasswordService, TokenService],
 })
 export class AuthModule {}
